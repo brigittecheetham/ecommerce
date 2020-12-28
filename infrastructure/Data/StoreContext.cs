69f0afbc3b89b04c7c1229dc4c2504698f38ea24
Brigitte <brigitte.cheetham@gmail.com>
2020-12-28 12:45:15 +0200

    Initial Commit

---
+++ infrastructure/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using core.Entities;
+
+namespace infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
